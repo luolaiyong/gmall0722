ac29dd84eda07ad4d1797727c0cfbb77574c546d
luoly_global <luolaiyong1@126.com>
2019-12-13 23:45:49 +0800

    first commit from 2.0.1

---
+++ gmall-search/src/main/java/GitCommitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author mark luo
+ * @version 1.0
+ * @date 2019/12/13 23:43
+ */
+public class GitCommitTest {
+    public void hello(){
+        System.out.println("I am from dev/2.0.1");
+    }
+}
