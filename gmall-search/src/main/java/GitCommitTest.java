0f18b054c2e591a2487333bf70295b1c554a1ab9
luoly_global <luolaiyong1@126.com>
2019-12-13 23:48:49 +0800

    third commit from 2.0.1

--- gmall-search/src/main/java/GitCommitTest.java
+++ gmall-search/src/main/java/GitCommitTest.java
@@ -5,7 +5,7 @@
  */
 public class GitCommitTest {
     public void hello(){
-        System.out.println("I am from dev/2.0.1");
+        System.out.println("I am from dev/2.0.1,two");
         System.out.println("I am a handsome boy");
     }
 }
