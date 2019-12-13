fb1d64589762230be16a651e6aeb3f46ad46a328
luoly_global <luolaiyong1@126.com>
2019-12-13 09:51:55 +0800

    third commit from 1.0.1

--- gmall-user/src/main/java/com/atguigu/git/UserAplication.java
+++ gmall-user/src/main/java/com/atguigu/git/UserAplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserAplication {
     public static void main(String[] args) {
+        System.out.println("测试让我改的");
         SpringApplication.run(UserAplication.class, args);
     }
 
