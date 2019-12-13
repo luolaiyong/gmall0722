30d6e6922f74f906385c7b9fb6873a00333c646c
luoly_global <luolaiyong1@126.com>
2019-12-14 00:00:27 +0800

    first commit from 2.0.2

---
+++ gmall-item/src/main/java/GitCommit.java
@@ -0,0 +1,10 @@
+/**
+ * @author mark luo
+ * @version 1.0
+ * @date 2019/12/13 23:59
+ */
+public class GitCommit {
+    public void hello(){
+        System.out.println("You are a smart boy");
+    }
+}
