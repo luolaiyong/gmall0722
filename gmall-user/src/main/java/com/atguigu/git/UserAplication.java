cfd55c6c0dce961de1cbb724a625ea2e96d76f23
luoly_global <luolaiyong1@126.com>
2019-12-13 09:42:24 +0800

    first commit from 1.0.1

---
+++ gmall-user/src/main/java/com/atguigu/git/UserAplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mark luo
+ * @version 1.0
+ * @date 2019/12/13 9:30
+ */
+@SpringBootApplication
+public class UserAplication {
+}
