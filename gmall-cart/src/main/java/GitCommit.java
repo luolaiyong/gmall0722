1048100be41f7bfc692a88e89f5fa0e36dd76bf8
luoly_global <luolaiyong1@126.com>
2019-12-14 14:26:01 +0800

    first commit from 4.0.1

--- gmall-cart/src/main/java/GitCommit.java
+++ gmall-cart/src/main/java/GitCommit.java
@@ -5,7 +5,7 @@
  */
 public class GitCommit {
     public void hello(){
-        System.out.println("你该添加购物车了");
+        System.out.println("你该添加购物车了,4.0.1");
         System.out.println("测试让我改的3.0.1");
     }
 }
