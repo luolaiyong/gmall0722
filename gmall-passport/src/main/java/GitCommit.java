37e57acdb647f5fb68e24f139d728cc58320fc3f
luoly_global <luolaiyong1@126.com>
2019-12-14 09:02:14 +0800

    second commit from 3.0.1 lly

---
+++ gmall-passport/src/main/java/GitCommit.java
@@ -0,0 +1,12 @@
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
+
+/**
+ * @author mark luo
+ * @version 1.0
+ * @date 2019/12/14 9:01
+ */
+public class GitCommit {
+    public void hello(){
+        System.out.println("你该登陆了");
+    }
+}
