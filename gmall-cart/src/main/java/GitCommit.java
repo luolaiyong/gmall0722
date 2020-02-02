5a017800df763ac302256180cf81f428840619f8
luoly_global <luolaiyong1@126.com>
2020-02-02 18:13:02 +0800

    second commit from 5.0.1

--- gmall-cart/src/main/java/GitCommit.java
+++ gmall-cart/src/main/java/GitCommit.java
@@ -7,8 +7,8 @@ public class GitCommit {
     public void hello(){
         System.out.println("你该添加购物车了,4.0.1");
         System.out.println("你该添加购物车了,3.0.1");
-        System.out.println("3.0.1");
-        System.out.println("测试让我改的3.0.1");
+        System.out.println("3.0.1,5.0.1");
+        System.out.println("测试让我改的3.0.1,5.0.1");
         System.out.println("尚硅谷5.0.1上线了");
     }
 }
