649c5f4e89194392e4ea2ca661a1417b4de56093
luoly_global <luolaiyong1@126.com>
2020-02-02 18:18:34 +0800

    third commit from 5.0.1

--- gmall-cart/src/main/java/GitCommit.java
+++ gmall-cart/src/main/java/GitCommit.java
@@ -6,9 +6,7 @@
 public class GitCommit {
     public void hello(){
         System.out.println("你该添加购物车了,4.0.1");
-        System.out.println("你该添加购物车了,3.0.1");
-        System.out.println("3.0.1,5.0.1");
-        System.out.println("测试让我改的3.0.1,5.0.1");
+        System.out.println("5.0.1新增");
         System.out.println("尚硅谷5.0.1上线了");
     }
 }
