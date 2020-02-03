58b49a5c1ac481ee56cacf76fb3f91b7576b81ef
luoly_global <luolaiyong1@126.com>
2020-02-03 14:17:19 +0800

    first commit from 6.0.1

---
+++ gmall-order/src/main/java/OrderController.java
@@ -0,0 +1,10 @@
+/**
+ * @author mark luo
+ * @version 1.0
+ * @date 2020/2/3 14:14
+ */
+public class OrderController {
+    public void submmit(){
+        System.out.println("提交订单完成6.0.1");
+    }
+}
