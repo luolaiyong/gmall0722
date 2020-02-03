c2d93bdad22c1d9043bdf86225a8e5c02e9c65f0
luoly_global <luolaiyong1@126.com>
2020-02-03 15:06:53 +0800

    third commit from 6.0.1

--- gmall-order/src/main/java/OrderController.java
+++ gmall-order/src/main/java/OrderController.java
@@ -7,5 +7,6 @@ public class OrderController {
     public void submmit(){
         System.out.println("提交订单完成6.0.1");
         System.out.println("尚硅谷电商订单模块上线了5.0.1");
+        System.out.println("测试让改的6.0.1");
     }
 }
