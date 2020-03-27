8bf67a6930522d94d4a1081c9988b20a5117e0fe
luoly_global <luolaiyong1@126.com>
2020-03-27 15:28:26 +0800

    second commit from 11.0.1

--- gmall-order/src/main/java/GitCommit.java
+++ gmall-order/src/main/java/GitCommit.java
@@ -7,5 +7,6 @@ public class GitCommit {
     public void hello(){
         System.out.println("你该提交订单了,7.0.1");
         System.out.println("你该提交订单了,6.0.1");
+        System.out.println("会有冲突嘛？我来试试11.0.1");
     }
 }
